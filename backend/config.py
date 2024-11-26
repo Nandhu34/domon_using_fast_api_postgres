c1ed0611482e68a27242bb14fe48d68daedf64d3
nandhakumar <nandhakumarselva2000@gmail.com>
2024-11-26 10:22:42 +0530

    checking git ignore

--- backend/config.py
+++
@@ -1,8 +0,0 @@
-mongo_connection_url ="mongodb://localhost:27017"
-database_name ="domain_monitoring"
-user_details_collection = "user_details"
-served_whois_data_collection ="client_served_data"
-total_registered_domains_collection = "total_registered_domains"
-subscription_details_collection = "subscription_details"
-# secrete 
-secret ="domain moniter"
