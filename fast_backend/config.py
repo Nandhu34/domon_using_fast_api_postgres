f2e95c0a1c5cd3ed07402b93bc6f77fd61850c92
nandha34 <nandhakumarselva2000@gmail.com>
2025-02-08 08:48:11 +0530

    docker for backed has been added

--- fast_backend/config.py
+++ fast_backend/config.py
@@ -1,6 +1,6 @@
 hostname = "127.0.0.1"
 port = 8000
-mongo_uri ='mongodb://localhost:27017'
+mongo_uri ='mongodb://mongo:unIzHWKNaHdsYQHIAWiatOsYBODcETaF@autorack.proxy.rlwy.net:42724'
 db_name="domain_monitering"
 user_details_coll = "user_details"
 total_registered_database_coll = "total_registered_domains"
